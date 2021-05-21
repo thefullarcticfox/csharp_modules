46b20a9035bdbfc91c90a8fb3746b73be9c06f7a
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-21 17:29:50 +0300

    day00

---
+++ Day00/Ex00/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+static void SeparateOutput() => Console.WriteLine(new string('-', 120));
+
+static double GetMonthlyRate(double yearlyRate) => yearlyRate / 12 / 100;
+
+static double GetAnnuityPayment(double remainingDebt, int term, double yearlyRate)
+{
+	double	monthlyRate = GetMonthlyRate(yearlyRate);
+	double monthlyRatePowByTerm = Math.Pow(1.0 + monthlyRate, term);
+	return (remainingDebt * monthlyRate * monthlyRatePowByTerm) / (monthlyRatePowByTerm - 1.0);
+}
+
+static int GetMonthsRecount(double payment, double yearlyRate, double remainingDebt)
+{
+	double	monthlyRate = GetMonthlyRate(yearlyRate);
+	double quotent = payment - monthlyRate * remainingDebt;
+	return (int)Math.Round(Math.Log(payment / quotent, 1.0 + monthlyRate));
+}
+
+static double GetPercents(double debtSum, double yearlyRate, DateTime month) =>
+	debtSum * yearlyRate * DateTime.DaysInMonth(month.Year, month.Month) / 100 /
+	(DateTime.IsLeapYear(month.Year) ? 366 : 365);
+
+static double GetOverpayAfterAddPayment(double remainingDebt, double annuityPayment, double rate,
+	double percents, int term, DateTime month, bool recountPercent = false)
+{
+	var overpay = 0.0;
+	while (--term >= 0)
+	{
+		var debtPaymentPart = 0.0;
+
+		if (recountPercent)	// we need to recount percent after annuityPayment in this month with specific output
+		{
+			debtPaymentPart = annuityPayment - percents;
+			remainingDebt -= debtPaymentPart;
+			month = month.AddMonths(1);
+			double nextAnnuityPayment = GetAnnuityPayment(remainingDebt, term, rate);
+			double nextPercent = GetPercents(remainingDebt, rate, month);
+			Console.WriteLine("{0:dd.MM.yyyy} | Payment: {1,12:N2} | Debt part: {2,12:N2} | Percents: {3,12:N2} | Remaining Debt: {4,12:N2}",
+				month, nextAnnuityPayment, debtPaymentPart, percents, remainingDebt);
+			overpay += annuityPayment;
+			annuityPayment = nextAnnuityPayment;
+			percents = nextPercent;
+			recountPercent = false;
+			--term;
+		}
+
+		month = month.AddMonths(1);
+		overpay += annuityPayment;
+		debtPaymentPart = annuityPayment - percents;
+		remainingDebt -= debtPaymentPart;
+		if (term == 0 && remainingDebt > 0.0)
+		{
+			overpay += remainingDebt;
+			debtPaymentPart += remainingDebt;
+			annuityPayment += remainingDebt;
+			remainingDebt = 0;
+		}
+		if (remainingDebt < 0)
+			remainingDebt = 0.0;
+		Console.WriteLine("{0:dd.MM.yyyy} | Payment: {1,12:N2} | Debt part: {2,12:N2} | Percents: {3,12:N2} | Remaining Debt: {4,12:N2}",
+			month, annuityPayment, debtPaymentPart, percents, remainingDebt);
+		percents = GetPercents(remainingDebt, rate, month);
+
+	}
+	return overpay;
+}
+
+if (args.Length < 5) {
+	Console.WriteLine("Input error. Check input data and retry.");
+	return;
+}
+
+double sum;
+double rate;
+int term;
+int selectedMonth;
+double payment;
+try
+{
+	sum = double.Parse(args[0]);
+	rate = double.Parse(args[1]);
+	term = int.Parse(args[2]);
+	selectedMonth = int.Parse(args[3]);
+	payment = double.Parse(args[4]);
+	if (rate < 0.0 || sum < 0.0 || term < 0 || payment < 0.0 ||
+	    selectedMonth < 0 || selectedMonth >= term)
+	{
+		throw new Exception();
+	}
+}
+catch (Exception)
+{
+	Console.WriteLine("Input error. Check input data and retry.");
+	return;
+}
+
+// counting formulas
+double annuityPayment = GetAnnuityPayment(sum, term, rate);
+double overpay = -sum;
+double remainingSum = sum;
+var month = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+var percents = 0.0;
+
+Console.WriteLine("{0:dd.MM.yyyy} | Payment: {1,12:N2} | Debt part: {2,12:N2} | Percents: {3,12:N2} | Remaining Debt: {4,12:N2}",
+	month, annuityPayment, sum, annuityPayment * term - sum, remainingSum);
+SeparateOutput();
+percents = GetPercents(remainingSum, rate, month);
+
+for (var i = 1; i < selectedMonth; i++)
+{
+	overpay += annuityPayment;
+	month = month.AddMonths(1);
+	double debtPaymentPart = annuityPayment - percents;
+	remainingSum -= debtPaymentPart;
+	if (remainingSum < 0)
+		remainingSum = 0;
+	Console.WriteLine("{0:dd.MM.yyyy} | Payment: {1,12:N2} | Debt part: {2,12:N2} | Percents: {3,12:N2} | Remaining Debt: {4,12:N2}",
+		month, annuityPayment, debtPaymentPart, percents, remainingSum);
+	percents = GetPercents(remainingSum, rate, month);
+	--term;
+}
+
+// test related default overpay after selected month
+double overpayDefault = GetOverpayAfterAddPayment(remainingSum, annuityPayment, rate, percents, term, month) + overpay;
+SeparateOutput();
+
+// additional payment
+overpay += payment;
+remainingSum -= payment;
+
+// percent/payment recount
+double overpayAfterPercentDecrease = GetOverpayAfterAddPayment(remainingSum,
+	annuityPayment, rate, percents, term, month, true) + overpay;
+SeparateOutput();
+
+// months recount
+term = GetMonthsRecount(annuityPayment, rate, remainingSum);
+double overpayAfterTermDecrease = GetOverpayAfterAddPayment(remainingSum,
+	annuityPayment, rate, percents, term, month) + overpay;
+SeparateOutput();
+
+// output
+Console.WriteLine("Overpay without any decrease:\t{0:N2} rubles", overpayDefault);
+Console.WriteLine("Overpay on payment decrease:\t{0:N2} rubles", overpayAfterPercentDecrease);
+Console.WriteLine("Overpay on term decrease:\t{0:N2} rubles", overpayAfterTermDecrease);
+
+double diff = Math.Abs(overpayAfterPercentDecrease - overpayAfterTermDecrease);
+if (diff < 1.0)
+{
+	Console.WriteLine("Overpay is the same.");
+}
+else if (overpayAfterPercentDecrease < overpayAfterTermDecrease)
+{
+	Console.WriteLine("Percent decrease is better than term decrease by {0:N2} rubles", diff);
+}
+else
+{
+	Console.WriteLine("Term decrease is better than percent decrease by {0:N2} rubles", diff);
+}
---
+++ Day00/Ex01/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+static int LevDist(string a, string b)
+{
+    if (a.Length == 0)
+        return a.Length;
+    if (b.Length == 0)
+        return b.Length;
+
+    int res0 = LevDist(a[1..], b[1..]);
+    if (char.ToLower(a[0]) == char.ToLower(b[0]))
+        return res0;
+
+    int res1 = LevDist(a[1..], b);
+    int res2 = LevDist(a, b[1..]);
+    return 1 + Math.Min(res0, Math.Min(res1, res2));
+}
+
+const string dictPath = "us.txt";
+if (!File.Exists(dictPath))
+    return;
+
+string[] dictionary = File.OpenText(dictPath).ReadToEnd().
+    Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+Console.WriteLine("> Enter name:");
+string? name = Console.ReadLine();
+if (name == null)
+    return;
+
+var minLevDist = int.MaxValue;
+var closestName = "";
+foreach (string word in dictionary)
+{
+    int levDist = LevDist(name, word);
+    // Console.WriteLine($"{name} - {word} = {levDist}");
+    if (minLevDist == levDist && Math.Abs(word.Length - name.Length) < Math.Abs(closestName.Length - name.Length) ||
+        minLevDist > levDist)
+    {
+        minLevDist = levDist;
+        closestName = word;
+
+        switch (minLevDist)
+        {
+            case 0:
+            {
+                Console.WriteLine($">Hello, {name}!");
+                return;
+            }
+            case < 3:
+            {
+                Console.WriteLine($">Did you mean “{closestName}”? Y/N");
+                string? response = Console.ReadLine();
+                if (response == "Y")
+                {
+                    name = closestName;
+                    Console.WriteLine($">Hello, {name}!");
+                    return;
+                }
+                break;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Name not found");
