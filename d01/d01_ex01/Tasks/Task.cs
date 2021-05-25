65077765a0cc0a9e74ef2aeb23f9b398b1185c43
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-25 19:16:28 +0300

    d01ex01 init tasks+

--- d01/d01_ex01/Tasks/Task.cs
+++ d01/d01_ex01/Tasks/Task.cs
@@ -4,11 +4,21 @@ namespace d01_ex01.Tasks
 {
     public struct Task
     {
-        public string Title;
-        public string Description;
-        public DateTime Deadline;
-        public TaskPriority Priority;
-        public TaskState State;
-        public TaskType Type;
+        private string _title;
+        private string _description;
+        private DateTime _deadline;
+        private TaskPriority _priority;
+        private TaskType _type;
+        private TaskState _state;
+
+        public Task(string title, string description, DateTime deadline, TaskPriority priority, TaskType type)
+        {
+            _title = title;
+            _description = description;
+            _deadline = deadline;
+            _priority = priority;
+            _type = type;
+            _state = TaskState.Created;
+        }
     };
 }
