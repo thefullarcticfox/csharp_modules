5c4a24afa67ac73f6abc1db4ec3c8425daa1d074
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-30 15:43:22 +0300

    d02 init

---
+++ d02/d02_ex00/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace d02_ex00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ d02/d02_ex01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace d02_ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
