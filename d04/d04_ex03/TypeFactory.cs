c8835ac316a92ca79ad8ec59d2814ced9d0721c9
Ivan <thefullarcticfox@users.noreply.github.com>
2021-06-16 17:53:45 +0300

    d04ex03 simplify creation with constructor+

--- d04/d04_ex03/TypeFactory.cs
+++ d04/d04_ex03/TypeFactory.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Reflection;
 
 namespace d04_ex03
 {
     public static class TypeFactory
     {
-        public static T CreateWithConstructor<T>() where T : class
-        {
-            ConstructorInfo ctorInfo = typeof(T).GetConstructor(Type.EmptyTypes);
-            return (T)ctorInfo?.Invoke(null);
-        }
+        public static T CreateWithConstructor<T>() where T : class =>
+            (T)typeof(T).GetConstructor(Type.EmptyTypes)?.Invoke(null);
 
         public static T CreateWithActivator<T>() where T : class =>
             (T)Activator.CreateInstance(typeof(T));
