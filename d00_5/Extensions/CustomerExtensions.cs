ec95a19dfd5e48e81ae25395b15398935c2672e6
Ivan <thefullarcticfox@users.noreply.github.com>
2021-06-21 21:49:15 +0300

    d00_5 update

--- d00_5/Extensions/CustomerExtensions.cs
+++ d00_5/Extensions/CustomerExtensions.cs
@@ -4,14 +4,14 @@ using d00_5.Models;
 
 namespace d00_5.Extensions
 {
-    static class CustomerExtensions
+    public static class CustomerExtensions
     {
-        public static CashRegister GetLeastQueueCashRegister(this Customer customer, List<CashRegister> cashRegisters) =>
+        public static CashRegister GetLeastQueueCashRegister(this Customer _, IEnumerable<CashRegister> cashRegisters) =>
             cashRegisters
                 .OrderBy(c => c.CustomerQueue.Count)
                 .FirstOrDefault();
 
-        public static CashRegister GetLeastProductsCashRegister(this Customer customer, List<CashRegister> cashRegisters) =>
+        public static CashRegister GetLeastProductsCashRegister(this Customer _, IEnumerable<CashRegister> cashRegisters) =>
             cashRegisters
                 .OrderBy(c => c.CustomerQueue.Sum(c => c.CartProductCount))
                 .FirstOrDefault();
