b71b268571776b6d66bc19e3bda0c960198e216e
Ivan <thefullarcticfox@users.noreply.github.com>
2021-06-26 20:05:04 +0300

    d05 first test

--- d05/Tests/Markdown.Generator.Core.Tests/ElementsTests.cs
+++ d05/Tests/Markdown.Generator.Core.Tests/ElementsTests.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using Markdown.Generator.Core.Markdown.Elements;
 
 namespace Markdown.Generator.Core.Tests
 {
     public class ElementsTests
     {
         [Fact]
-        public void Test1()
+        public void Given_Code_When_LanguageAndCodeAsParameter_Then_ReturnMarkdownCodeMarkup()
         {
+            // arrange
+            var expected = "```csharp\nsome code\n```\n";
+            var code = new Code("csharp", "some code");
+            
+            // act
+            var actual = code.Create();
 
+            // assert
+            Assert.Equal(expected, actual);
         }
     }
 }
